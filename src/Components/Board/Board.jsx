d8355cc19587153f3ee0b7987201ae180b0c5220
Rupam <rupammondal564@gmail.com>
2024-08-02 08:24:50 +0530

    Update

--- src/Components/Board/Board.jsx
+++ src/Components/Board/Board.jsx
@@ -1,9 +1,12 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from "react";
+import Button from "../Button/Button";
 import './board.css'
-import Button from '../Button/Button'
 
 function Board(){
-
+    const [board , setBoard] = useState(Array(9).fill(null))
+    const [turnX , setTurn] = useState(true)
+    const [winner , setWinner] = useState(false)
+    const [winnerprint, setPrint] = useState(null)
     const patterns = [
         [0, 1, 2],
         [3, 4, 5],
@@ -14,67 +17,54 @@ function Board(){
         [0, 4, 8],
         [2, 4, 6]
     ]
-    const [board , setBoard] = useState(Array(9).fill(null))
-    const [turnX , setTurn] = useState(true)
-    const [winner , setWinner] = useState(null)
-    const [winnerprint , setPrint] = useState(null)
-
-
     useEffect(() => {
         const ans = checkWinner()
         if(ans){
-            setPrint(`Winner is = ${ans}`)
+            console.log("winner is = "+ans)
+            setPrint("Winner is = "+ans)
         }
-    }, [board])
-
+    } , [board])
     function checkWinner(){
-
         for(let pat of patterns){
             const [a , b , c] = pat
-            if(board[a] != null && board[b] != null && board[c] != null){
-                if(board[a] == board[b] && board[b] == board[c]){
-                    setWinner(board[a])
-                    console.log(board[a])
-                    return board[a]
-                }
+            if(board[a] && board[a] == board[b] && board[b] == board[c]){
+                setWinner(true)
+                return board[a];
             }
         }
-        return null
     }
-
-    function reserGame(){
-        setBoard(Array(9).fill(null))
-        setWinner(null)
-        setTurn(true)
-        setPrint(null)
-    }
-
     function Toggle(index){
-        if(board[index] || winner)return
+        if(board[index] || winner) return
 
         const newBoard = [...board]
-        newBoard[index] = turnX ? 'X':'O'
+        newBoard[index] = turnX ? 'X' : 'O'
         setBoard(newBoard)
-        
+
         setTurn(!turnX)
     }
-    return (
+    function resetGame(){
+        setBoard(Array(9).fill(null))
+        setTurn(true)
+        setWinner(false)
+        setPrint(null)
+    }
+    return(
         <>
-            <div className='w-96 h-16 mb-5 flex justify-center items-center font-mono text-3xl'>{winnerprint}</div>
-            <div className='board'>
+            <div className='w-96 h-16 mb-5 flex justify-center items-center font-mono text-3xl text-white'>{winnerprint}</div>
+            <div className="board">
                 {
                     board.map((v , i) => (
                         <Button
-                            value={v}
-                            onClick={() => {
-                                Toggle(i)
-                            }}
-                            key={i}
+                        value={v}
+                        onClick={() => {
+                            Toggle(i)
+                        }}
+                        key={i}
                         />
                     ))
                 }
             </div>
-            <button className='h-10 w-24 mt-6 bg-slate-500 rounded-md' onClick={reserGame}>Reset Game</button>
+            <button className='h-10 w-24 mt-6 bg-white rounded-md' onClick={resetGame}>Reset Game</button>
         </>
     )
 }
